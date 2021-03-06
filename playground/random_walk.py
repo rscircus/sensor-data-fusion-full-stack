480c119aeddd2196aab7e76541b6c6083bbfb11c
Roland Siegbert <roland@siegbert.info>
2021-03-06 14:54:39 +0100

    Add noisy 1d movement with const velocity

--- playground/random_walk.py
+++ playground/random_walk.py
@@ -10,14 +10,47 @@ import matplotlib.pyplot as plt
 # 1D first:
 # %% - Setup distribution to be sampled
 
+
+def generate_1d_noisy_movement(x_start=0, x_vel=1.0, num_steps=100, pause_ratio=0.5):
+    """Generates a linear noisy movement."""
+
+    DOWN = 0
+    UP = 1
+
+    # 1D first
+
+    up_prob = pause_ratio
+
+    start = x_start
+    positions = [start]
+
+    # generate a bunch of random numbers and spread them
+    # 1.0 - up_prob - down_prob of the time we do not move
+    sample = np.random.random(num_steps)
+    up_samples = sample > 1.0 - up_prob
+
+    for up in up_samples:
+        positions.append(positions[-1] + up / pause_ratio)
+
+    return positions
+
+
+positions = generate_1d_noisy_movement()
+plt.plot(positions)
+
+# %%
+# 1D random walk:
+
+
 DOWN = 0
 UP = 1
 
 # 1D first
+
 up_prob = 0.05
 down_prob = up_prob
 
-start = 10
+start = 0
 positions = [start]
 num_steps = 1000
 
@@ -30,7 +63,6 @@ up_samples = sample > 1.0 - up_prob
 for down, up in zip(down_samples, up_samples):
     positions.append(positions[-1] - down + up)
 
-plt.plot(positions)
 
 # 2D now:
 
